a2c14110e44f775e62aa38a4b06454ffa49cace6
Alex Beals <alexcbeals@gmail.com>
2022-09-18 20:25:11 -0700

    Use nonsense

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from utils import hex_to_hash, hash_to_hex
+from utils import hex_to_hash, hash_to_hex, ioi_string_to_hex
 
 class UtilsTest(unittest.TestCase):
 
@@ -10,6 +10,10 @@ class UtilsTest(unittest.TestCase):
     def test_hash_to_hex(self):
         self.assertEqual(hash_to_hex(9925485480809987), '0023432D2C4E9603')
         self.assertEqual(hash_to_hex(5935644050841452), '0015166FD329DF6C')
+
+    def test_ioi_string_to_hex(self):
+        self.assertEqual(ioi_string_to_hex('[assembly:/_pro/environment/textures/constants/color_black.texture?/diffuse_a.tex](ascolormap).pc_tex'), '006BC76DF396C49E')
+        self.assertEqual(ioi_string_to_hex('[[assembly:/_pro/environment/materialclasses/basicmaterial.materialclass].fx](dx11).pc_mate'), '00E7D648A187A9C1')
 
 if __name__ == '__main__':
     unittest.main()
--- utils.py
+++ utils.py
@@ -7,4 +7,5 @@ def hex_to_hash(hex: str) -> int:
     return int(hex.lstrip('0'), 16)
 
 def ioi_string_to_hex(path: str) -> str:
-    return hashlib.md5(path.encode()).hexdigest()
+    raw = hashlib.md5(path.encode()).hexdigest().upper()
+    return '00' + raw[2:16]

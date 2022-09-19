18b72bcc3088abb7aa26083ce14e099df9076395
Alex Beals <alexcbeals@gmail.com>
2022-09-18 21:50:43 -0700

    Build setup.py file and flesh out README

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
-import pickle
+import pickle, os, requests, json, zipfile
 from typing import Dict
 from utils import hex_to_hash
+
+if os.path.exists('./rpkg'):
+    print("rpkg folder already exists. If you want to update, just delete it.")
+    exit()
+
+# Download rpkg
+rpkg_tool_info = json.loads(requests.get('https://api.github.com/repos/glacier-modding/RPKG-Tool/releases/latest').text)
+download_links = [x['browser_download_url'] for x in rpkg_tool_info['assets'] if 'src' not in x['name']]
+if (len(download_links) != 1):
+    print("Something went wrong with downloading rpkg.")
+    exit()
+
+r = requests.get(download_links[0], stream=True)
+with open('rpkg.zip', 'wb') as fd:
+    for chunk in r.iter_content(chunk_size=128):
+        fd.write(chunk)
+
+with zipfile.ZipFile('rpkg.zip') as zf:
+    zf.extractall('rpkg')
+
+os.remove('rpkg.zip')
 
 # This is intended to be run just once whenever hash_list.txt is updated.
 with open('./rpkg/hash_list.txt', 'r') as f:

6915fb56d44d49d20bceab6879ef9dd295a0960e
Alex Beals <alexcbeals@gmail.com>
2022-09-18 19:26:50 -0700

    Regex search

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from os import listdir
 from os.path import isfile, join
 from extract import extract
 from search import search
+import re
 
 # File Directory
 directory = "D:\\Program Files (x86)\\Epic Games\\HITMAN3\\Runtime"
 
 # Open the directory and determine all of the possible rkpg files
 rpkgs_names = [f for f in listdir(directory) if isfile(join(directory, f)) and f.endswith('.rpkg')]
-print(rpkgs_names)
 # TODO: Just for testing
-rpkgs_names = ['chunk21patch2.rpkg']
-# rpkgs_names = ['chunk24.rpkg', 'chunk3.rpkg']
+# rpkgs_names = ['chunk21patch2.rpkg']
+rpkgs_names = ['chunk24.rpkg', 'chunk3.rpkg']
 
 for rpkg_name in rpkgs_names:
     rpkg_path = join(directory, rpkg_name)
@@ -19,7 +19,8 @@ for rpkg_name in rpkgs_names:
 
     # for i in rpkg.hashes:
     #     print(rpkg.hashes[i].getHexName())
-    search(rpkg, 'moose')
+    pattern = re.compile("assembly:.*paintings.*diffuse_.*")
+    search(rpkg, pattern, 'TEXT')
 
 # # For each rpkg search specifically for TEXT that matches
 # list_files = subprocess.run(
--- search.py
+++ search.py
@@ -1,16 +1,22 @@
 from RPKG import RPKG
-import pickle
+import pickle, re
+from typing import Optional, Pattern
 
 with open('hashes/mapping.pickle', 'rb') as handle:
     mapping = pickle.load(handle)
 with open('hashes/no_name.pickle', 'rb') as handle:
     no_name = pickle.load(handle)
 
-def search(rpkg: RPKG, search_string: str):
-    for k in mapping:
-        if search_string in k:
-            hex_file_path = mapping[k]
+def search(rpkg: RPKG, search_string: Pattern[str], type: Optional[str] = None):
+    for ioi_string in mapping:
+        if re.search(search_string, ioi_string):
+            hex_file_path = mapping[ioi_string]
             for i in rpkg.hashes:
                 if rpkg.hashes[i].getHexName() == hex_file_path:
-                    print(rpkg.hashes[i], i, hex_file_path)
+                    if type is None:
+                        matches_type = True
+                    else:
+                        matches_type = (rpkg.hashes[i].hash_resource_type == type)
+                    if matches_type:
+                        print(ioi_string, i, hex_file_path)
     return None

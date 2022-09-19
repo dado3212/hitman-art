15e09b570e3cca1c0aca8868b8e968e32a1ccf18
Alex Beals <alexcbeals@gmail.com>
2022-09-18 17:37:06 -0700

    Start of swing

--- extract.py
+++ extract.py
@@ -1,6 +1,10 @@
 from os import listdir
 from os.path import isfile, join, getsize
+from typing import List, Dict, Tuple
 import subprocess
+
+# Some useful types
+Hash = Tuple[int, int, int]
 
 # File Directory
 directory = "D:\\Program Files (x86)\\Epic Games\\HITMAN3\\Runtime"
@@ -75,72 +79,59 @@ else:
         # Default value
         is_patch_file = False
     
-print(is_patch_file)
-print(file_size)
+if (is_patch_file):
+    if (patch_count > 0):
+        f.seek(patch_offset)
+        patch_entry_list: List[int] = []
+        for _ in range(0, patch_count):
+            patch_entry_list.append(int.from_bytes(f.read(8), 'little'))
+        print(patch_entry_list)
+
+# Seek to the hash data table's offset
+hash_data_offset = 0x10
+if (version == 2):
+    hash_data_offset += 9
+if (is_patch_file):
+    hash_data_offset += patch_count * 8 + 4
+
+f.seek(hash_data_offset)
+
+# Read both RPKG's hash tables at once into a temporary char buffer
+hashes: List[Hash] = []
+hash_map: Dict[int, int] = dict()
+for i in range(0, hash_count):
+    # Create a new hash
+    # uint64_t hash = 0;
+    # uint64_t data_offset = 0;
+    # uint32_t data_size = 0;
+    hash = int.from_bytes(f.read(8), 'little')
+    data_offset = int.from_bytes(f.read(8), 'little')
+    data_size = int.from_bytes(f.read(4), 'little')
+
+    hash_map[hash] = i
+    hashes.append((hash, data_offset, data_size))
+
+for i in range(hash_count):
+    # Create a hash resource
+    # char resource_type[4];
+    # uint32_t reference_table_size = 0;
+    # uint32_t reference_table_dummy = 0;
+    # uint32_t size_final = 0;
+    # uint32_t size_in_memory = 0;
+    # uint32_t size_in_video_memory = 0;
+
+    # TODO: This feels just outright wrong
+    resource_type_bytes: List[bytes] = []
+    for _ in range(4):
+        resource_type_bytes.append(f.read(1))
+    resource_type = "".join([x.decode("utf-8") for x in resource_type_bytes[::-1]])
+    print(resource_type)
+    reference_table_size = int.from_bytes(f.read(4), 'little')
+    reference_table_dummy = int.from_bytes(f.read(4), 'little')
+    size_final = int.from_bytes(f.read(4), 'little')
+    size_in_memory = int.from_bytes(f.read(4), 'little')
+    size_in_video_memory = int.from_bytes(f.read(4), 'little')
 '''
-
-    // Read in the patch list if RPKG is a patch file with a non zero patch list
-    if (rpkgs.back().is_patch_file)
-    {
-        if (rpkgs.back().header.patch_count > 0)
-        {
-            rpkg_file.SeekTo(patch_offset);
-            rpkgs.back().patch_entry_list.resize(rpkgs.back().header.patch_count);
-            rpkg_file.Read<uint64_t>(rpkgs.back().patch_entry_list.data(), rpkgs.back().header.patch_count);
-        }
-    }
-
-    std::string import_text = "Importing RPKG file: " + rpkgs.back().rpkg_file_name;
-    LOG_NO_ENDL(import_text);
-
-    // Seek to the hash data table's offset
-    size_t hash_data_offset = 0x10;
-    if (rpkgs.back().rpkg_file_version == 2)
-        hash_data_offset += 9;
-    if (rpkgs.back().is_patch_file)
-        hash_data_offset += rpkgs.back().header.patch_count * 8 + 4;
-
-    rpkg_file.SeekTo(hash_data_offset);
-
-    // Read both RPKG's hash tables at once into a temporary char buffer
-    size_t hash_tables_combined_size = rpkgs.back().header.hash_header_table_size + rpkgs.back().header.hash_resource_table_size;
-    std::vector<char> hash_tables(hash_tables_combined_size, 0);
-    rpkg_file.Read<char>(hash_tables.data(), hash_tables_combined_size);
-
-    // Load RPKG's hash tables in StreamReader
-    StreamReader hash_tables_stream(hash_tables.data(), hash_tables.size());
-
-    // Init timer
-    std::chrono::time_point start_time = std::chrono::high_resolution_clock::now();
-    int stringstream_length = 80;
-
-    // Read in the hash files/resources data tables
-    for (size_t i = 0; i < rpkgs.back().header.hash_count; i++)
-    {
-        // Emplace back new hash in rpkg
-        rpkgs.back().hash.emplace_back();
-
-        hash_tables_stream.Read<hash::HashHeader>(&rpkgs.back().hash.back().data.header);
-
-        rpkgs.back().hash_map[rpkgs.back().hash.back().data.header.hash] = (uint64_t)rpkgs.back().hash_map.size();
-    }
-
-    // Emplace back the hash depends map
-    hashes_depends_map.emplace_back();
-
-    // Read in the hash files/resources info tables
-    for (size_t i = 0; i < rpkgs.back().header.hash_count; i++)
-    {
-        //HashInfo hash_info;
-        hash_tables_stream.Read<hash::HashResource>(&rpkgs.back().hash[i].data.resource);
-
-        // Reverse the hash's resource type string
-        char type[4];
-        std::memcpy(&type, rpkgs.back().hash[i].data.resource.resource_type, 4);
-        rpkgs.back().hash[i].data.resource.resource_type[0] = type[3];
-        rpkgs.back().hash[i].data.resource.resource_type[1] = type[2];
-        rpkgs.back().hash[i].data.resource.resource_type[2] = type[1];
-        rpkgs.back().hash[i].data.resource.resource_type[3] = type[0];
 
         // Determine hash's size and if it is LZ4ed and/or XORed
         if ((rpkgs.back().hash[i].data.header.data_size & 0x3FFFFFFF) != 0)
